c99643544d6fc04bdbcdfe091e1a1bc5fa7b51aa
lupenq <skinilya@gmail.com>
2020-04-12 19:18:00 +0300

    Added mongoose

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const express = require('express')
 const path = require('path')
 const exphbs = require('express-handlebars')
+const handlebars = require('handlebars')
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+const mongoose = require('mongoose')
 const homeRoutes = require('./routes/home')
 const addRoutes = require('./routes/add')
 const coursesRoutes = require('./routes/courses')
 const cartRoutes = require('./routes/cart')
 
+require('dotenv').config();
 
 const app = express()
 
 const hbs = exphbs.create({
   defaultLayout: 'main',
-  extname: 'hbs'
+  extname: 'hbs',
+  handlebars: allowInsecurePrototypeAccess(handlebars)
 })
 
 app.engine('hbs', hbs.engine)
@@ -27,6 +32,23 @@ app.use('/cart', cartRoutes)
 
 const PORT = process.env.PORT || 3001
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`)
-})
+async function start() {
+  try {
+    const url = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0-5tn2x.mongodb.net/shop`
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    })
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port: ${PORT}`)
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
+
+
