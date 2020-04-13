8ac9b59126e621036e2e0ac1500be782ca96077f
lupenq <skinilya@gmail.com>
2020-04-13 21:03:07 +0300

    Validated course create form

--- routes/courses.js
+++ routes/courses.js
@@ -1,6 +1,8 @@
 const {Router} = require('express')
 const Course = require('../models/course')
 const auth = require('../middleware/auth')
+const {courseValidators} = require('../utils/validators')
+const {validationResult} = require('express-validator/check')
 
 const router = Router()
 
@@ -42,7 +44,14 @@ router.get('/:id/edit', auth, async (req, res) => {
   }
 })
 
-router.post('/edit', auth, async (req, res) => {
+router.post('/edit', auth, courseValidators, async (req, res) => {
+  const errors = validationResult(req)
+  const {id} = req.body
+
+  if (!errors.isEmpty()) {
+    return res.status(422).redirect(`/courses/${id}/edit?allow=true`)
+  }
+
   try {
     const {id} = req.body
     delete req.body.id
