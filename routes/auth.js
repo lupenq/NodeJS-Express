f8c1876af91869119f21403648f9bca1f7c4688d
lupenq <skinilya@gmail.com>
2020-04-13 16:23:18 +0300

    Connect login to mongodb

--- routes/auth.js
+++ routes/auth.js
@@ -17,15 +17,32 @@ router.get('/logout', async (req, res) => {
 })
 
 router.post('/login', async (req, res) => {
-  const user = await User.findById('5e93534ba15188bdde9e4194')
-  req.session.user = user
-  req.session.isAuthenticated = true
-  req.session.save(err => {
-    if (err) {
-      throw err
+  try {
+    const {email, password} = req.body
+
+    const candidate = await User.findOne({ email })
+
+    if (candidate) {
+      const areSame = password === candidate.password
+
+      if (areSame) {
+        req.session.user = candidate
+        req.session.isAuthenticated = true
+        req.session.save(err => {
+          if (err) {
+            throw err
+          }
+          res.redirect('/')
+        })
+      } else {
+        res.redirect('/auth/login')
+      }
+    } else {
+      res.redirect('/auth/login')
     }
-    res.redirect('/')
-  })
+  } catch (e) {
+    console.log(e)
+  }
 })
 
 router.post('/register', async (req, res) => {
